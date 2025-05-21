e5bb72d1ae597c5632dd11e118f4e75b4c02aaa5
mangcecep <cecepsolihinyusup@gmail.com>
2025-05-21 07:37:57 +0700

    feat: register modules

--- server/controllers/auth_ctrl.js
+++ server/controllers/auth_ctrl.js
@@ -1,8 +1,17 @@
 const { validationResult } = require("express-validator")
+const {
+    user,
+    student,
+    student_user,
+    role,
+    role_user
+} = require("../models")
+const bcrypt = require("bcryptjs")
+const { where } = require("sequelize")
 
 let self = {}
 
-self.save = (req, res) => {
+self.save = async (req, res) => {
 
     const error = validationResult(req)
     if (!error.isEmpty()) {
@@ -21,9 +30,44 @@ self.save = (req, res) => {
             major_id
         } = req.body
 
+        const passwordHash = await bcrypt.hash(password, 10)
+
+        const userData = await user.create({
+            username: username,
+            email: email,
+            password: passwordHash,
+        })
+
+        const studentData = await student.create({
+            firstName: firstName,
+            lastName: lastName,
+            classes: classes,
+            gender: gender,
+            major_id: major_id,
+        })
+
+        await student_user.create({
+            user_id: userData.id,
+            student_id: studentData.id
+        })
+
+        const roleUserRegister = await role.findOne({
+            where: {
+                role_name: "Student"
+            },
+            attributes: ["id"]
+        })
+
+        await role_user.create({
+            user_id: userData.id,
+            role_id: roleUserRegister.id
+        })
+
         res.status(201).json({
             message: "Register Success!",
+            data: userData
         })
+
     } catch (error) {
         res.status(500).json(error)
     }
